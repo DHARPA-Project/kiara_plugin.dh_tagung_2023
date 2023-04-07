cb19bcc6aada389d99f01fdc4e6a9c2971e0ce17
Caitlin Burge <caitlin.burge@uni.lu>
2023-04-07 13:40:56 +0200

    NA notebook

---
+++ src/kiara_plugin/dh_tagung_2023/modules/Network_Analysis/cutpoints_module.py
@@ -0,0 +1,66 @@
+from kiara.api import KiaraModule
+import networkx as nx
+from kiara_plugin.network_analysis.models import NetworkData
+
+KIARA_METADATA = {
+    "authors": [
+        {"name": "Caitlin Burge", "email": "caitlin.burge@uni.lu"},
+    ],
+    "description": "Kiara modules for: network_analysis",
+}
+
+class CutPointsList(KiaraModule):
+    """Create a list of nodes that are cut-points.
+    Cut-points are any node in a network whose removal disconnects members of the network, creating one or more new distinct components.
+    
+    Uses networkx.articulation_points()
+    https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.components.articulation_points.html"""
+    
+    _module_type_name = 'create.cut_point_list'
+
+    def create_inputs_schema(self):
+        return {
+            "network_data": {
+                "type": "network_data",
+                "doc": "The network graph being queried."
+            }
+        }
+
+    def create_outputs_schema(self):
+        return {
+            "network_result": {
+                "type": "list",
+                "doc" : "A list of all nodes that are cut-points."
+            },
+            "cut_network": {
+                "type": "network_data",
+                "doc": "Updated network data with cut-point 'Yes' or 'No' assigned as a node attribute."
+            }
+        }
+
+    def process(self, inputs, outputs):
+        edges = inputs.get_value_obj('network_data')
+
+        network_data: NetworkData = edges.data  # check the source for the NetworkData class to see what
+                                                # convenience methods it can give you:
+                                                # https://github.com/DHARPA-Project/kiara_plugin.network_analysis/blob/develop/src/kiara_plugin/network_analysis/models.py#L52
+
+        G = network_data.as_networkx_graph(nx.Graph)  # you can also use nx.DiGraph or other types
+
+        cutpoints = list(nx.articulation_points(G))
+        
+        cut_dict = {}
+        for node in G:
+            if node in cutpoints:
+                cut_dict[node] = 'Yes'
+            else:
+                cut_dict[node] = 'No'
+                
+        nx.set_node_attributes(G, cut_dict, 'Cut Point')
+
+        attribute_network = NetworkData.create_from_networkx_graph(G)
+        
+        outputs.set_values(network_result=cutpoints, cut_network=attribute_network)
+
+
+
---
+++ src/kiara_plugin/dh_tagung_2023/modules/Network_Analysis/modularity.py
@@ -0,0 +1,81 @@
+from kiara.api import KiaraModule, ValueMapSchema, ValueMap
+import networkx as nx
+from networkx.algorithms import community
+from operator import itemgetter
+
+from kiara_plugin.network_analysis.models import NetworkData
+
+KIARA_METADATA = {
+    "authors": [
+        {"name": "Lena Jaskov", "email": "helena.jaskov@uni.lu"},
+    ],
+    "description": "Kiara modules for: network_analysis",
+}
+
+class ModularityCommunity(KiaraModule):
+    """Calculate modularity for each node and attach modularity group number to node list as attribute.
+
+    This networkX based function uses Clauset-Newman-Moore greedy modularity maximization to find the community partition with the largest modularity.
+
+    Modularity community is a density-based community detection method that investigates the structural composition of a network.
+    """
+    
+    _module_type_name = 'compute.modularity_group'
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        result = {
+            "network_data": {
+                "type": "network_data",
+                "doc": "The network data to analyze.",
+                "optional": False,
+            },
+            "number_of_communities":{
+                "type": "integer",
+                "doc": "Number of communities into which the network should be partitioned. This is a user defined number that can be above or below maximum modularity.",
+                "optional": True,
+            }
+        }
+        return result
+    
+    def create_outputs_schema(self):
+        return {
+            "modularity_network": {
+                "type": "network_data",
+                "doc": "Updated network data with modularity group assigned as a node attribute."
+            },
+            "maximum_modularity":{
+                "type": "integer",
+                "doc": "The number of communities at which maximum modularity is reached for this network. If the 'number_of_communities' is manually set, this number might deviate from the manually computed number.",
+            }
+        }
+    
+    def process(self, inputs, outputs):
+        edges = inputs.get_value_obj('network_data')
+
+        network_data: NetworkData = edges.data
+
+        G = network_data.as_networkx_graph(nx.Graph) #Other graph types will work as well and modularity group distribution can be different with different graph types. Maximum modularity can also be affected by edge weight, but this parameter is currently not set. If the graph consists on many unconnected components, then the modularity groups will mostly coincide with the components. It would then make sense to extract the largest connected component first and to run the modularity module on the largest component.
+
+        number_of_communities = inputs.get_value_data("number_of_communities") #TODO: This integer must be in range [1, G.number_of_nodes()]; otherwise it will throw a networkX error.
+
+        if number_of_communities is None:
+            communities = community.greedy_modularity_communities(G)
+            maximum_modularity = len(communities)
+        else:
+            communities = community.greedy_modularity_communities(G, cutoff=number_of_communities, best_n=number_of_communities)
+            max_communities = community.greedy_modularity_communities(G)
+            maximum_modularity = len(max_communities)
+
+        modularity_dict = {} 
+        for i,c in enumerate(communities): # Loop through the list of communities, keeping track of the number for the community
+            for name in c: # Loop through each node in a community
+                modularity_dict[name] = i # Create an entry in the dictionary for the node, where the value is which group it belongs to.
+
+        nx.set_node_attributes(G, modularity_dict, 'modularity_group')
+
+        attribute_network = NetworkData.create_from_networkx_graph(G)
+        
+        outputs.set_values(modularity_network=attribute_network, maximum_modularity=maximum_modularity)
